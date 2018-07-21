8326e6330b6c8a42d8aa0afb97b5aa221df83d48
Lounah <30981413+Lounah@users.noreply.github.com>
2018-07-21 14:03:43 +0300

    added data package

--- app/src/main/java/combruce_willis/github/swipper/ui/MainActivity.kt
+++ app/src/main/java/combruce_willis/github/swipper/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package combruce_willis.github.swipper
+package combruce_willis.github.swipper.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import combruce_willis.github.swipper.R
 
 class MainActivity : AppCompatActivity() {
 
