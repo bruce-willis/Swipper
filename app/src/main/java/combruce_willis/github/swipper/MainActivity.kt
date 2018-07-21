4711da28d081bcdf7015695c743bfb8a5a3633ed
moonchild94 <moonchild174@gmail.com>
2018-07-21 14:05:36 +0300

    base screens

--- app/src/main/java/combruce_willis/github/swipper/MainActivity.kt
+++ app/src/main/java/combruce_willis/github/swipper/MainActivity.kt
@@ -1,12 +1,29 @@
 package combruce_willis.github.swipper
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LauncherFragment.OnScreenOpenListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeFragment(LauncherFragment.newInstance())
+    }
+
+    override fun onGameScreenOpen() {
+        changeFragment(GameFragment.newInstance())
+    }
+
+    override fun onTutorialScreenOpen() {
+        changeFragment(TutorialFragment.newInstance())
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
